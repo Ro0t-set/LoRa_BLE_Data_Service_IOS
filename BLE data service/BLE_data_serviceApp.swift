1ac160590ff6726dd73f3f7a52fd37898923cb52
Ro0t-set <tommaso.patriti@studio.unibo.it>
2022-04-26 16:04:54 +0200

    Initial Commit

---
+++ BLE data service/BLE_data_serviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLE_data_serviceApp.swift
+//  BLE data service
+//
+//  Created by Tommaso Patriti on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct BLE_data_serviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
